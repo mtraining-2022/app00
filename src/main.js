0680e501a112eb4d05d087be6005bb47706bcfff
training-2022 <training-2022@mail.ru>
2022-07-19 16:30:04 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    webPreferences: {
+        webviewTag: true,
+        nodeIntegration: true,
+        contextIsolation: false,
+    }
+  });
+
+  //mainWindow.loadURL('http://windy.com');
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+
